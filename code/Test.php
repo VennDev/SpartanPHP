58fbb4d92c66442fecb26b6722d33969c1d77ac9
VennV <111500380+VennDev@users.noreply.github.com>
2024-03-22 01:25:43 +0700

    Delete code directory

--- code/Test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (extension_loaded('spartan')) {
-    echo "Extension '$extensionName' is loaded\n";
-} else {
-    echo "Extension '$extensionName' is not loaded\n";
-}
-
-runSpartan(file_get_contents('test.spartan'));
